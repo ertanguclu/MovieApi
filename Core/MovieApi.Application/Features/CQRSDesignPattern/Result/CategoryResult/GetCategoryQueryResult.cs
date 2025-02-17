7f562d722384cf77a30cae09c02b90844b5e4bd8
Ertan GÜÇLÜ <[ ertan.guclu@std.yildiz.edu.tr]>
2025-02-17 12:02:20 +0300

    CQRS Tasarım Deseninin Uygulanması

---
+++ Core/MovieApi.Application/Features/CQRSDesignPattern/Result/CategoryResult/GetCategoryQueryResult.cs
@@ -0,0 +1,8 @@
+﻿namespace MovieApi.Application.Features.CQRSDesignPattern.Result.CategoryResult
+{
+    public class GetCategoryQueryResult
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
