aac73c9a283fbd982d76b160be61fd9072523500
Ertan GÜÇLÜ <[ ertan.guclu@std.yildiz.edu.tr]>
2025-02-17 12:23:02 +0300

    Handler Metotlarının Yazılması

---
+++ Core/MovieApi.Application/Features/CQRSDesignPattern/Handlers/MovieHandlers/UpdateMovieCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using MovieApi.Application.Features.CQRSDesignPattern.Commands.MovieCommands;
+using MovieApi.Persistence.Context;
+
+namespace MovieApi.Application.Features.CQRSDesignPattern.Handlers.MovieHandlers
+{
+    public class UpdateMovieCommandHandler
+    {
+        private readonly MovieContext _context;
+
+        public UpdateMovieCommandHandler(MovieContext context)
+        {
+            _context = context;
+        }
+        public async void Handle(UpdateMovieCommand command)
+        {
+            var movie = await _context.Movies.FindAsync(command.MovieId);
+            movie.Rating = command.Rating;
+            movie.Status = command.Status;
+            movie.Duration = command.Duration;
+            movie.CoverImageUrl = command.CoverImageUrl;
+            movie.CreatedYear = command.CreatedYear;
+            movie.Description = command.Description;
+            movie.ReleaseDate = command.ReleaseDate;
+            movie.Title = command.Title;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
