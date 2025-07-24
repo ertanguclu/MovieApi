f95f83239f84c6e6751b8340a847a56a022509ac
Ertan GÜÇLÜ <[ ertan.guclu@std.yildiz.edu.tr]>
2025-07-24 19:18:44 +0300

    Register with API on CQRS

---
+++ Core/MovieApi.Application/Features/CQRSDesignPattern/Handlers/UserRegisterHandlers/CreateUserRegisterCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using MovieApi.Application.Features.CQRSDesignPattern.Commands.UserRegisterCommands;
+using MovieApi.Persistence.Context;
+using MovieApi.Persistence.Identity;
+
+namespace MovieApi.Application.Features.CQRSDesignPattern.Handlers.UserRegisterHandlers
+{
+    public class CreateUserRegisterCommandHandler
+    {
+        private readonly MovieContext _context;
+        private readonly UserManager<AppUser> _userManager;
+
+        public CreateUserRegisterCommandHandler(MovieContext context, UserManager<AppUser> signInManager)
+        {
+            _context = context;
+            _userManager = signInManager;
+        }
+
+        public async Task Handle(CreateUserRegisterCommand command)
+        {
+            var user = new AppUser
+            {
+                UserName = command.Username,
+                Email = command.Email,
+                Name = command.Name,
+                Surname = command.Surname
+            };
+            await _userManager.CreateAsync(user, command.Password);//Şifre hashleme işlemi Identity tarafından yapılır.bu yüzden appuser dahil edilmez.
+
+        }
+    }
+}
