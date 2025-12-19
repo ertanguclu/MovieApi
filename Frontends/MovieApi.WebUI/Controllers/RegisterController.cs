f190cfcdea16f87c7b98ec23a720616ff1b7654f
Ertan GÜÇLÜ <[ ertan.guclu@std.yildiz.edu.tr]>
2025-12-19 15:20:44 +0300

    Added API Registration Process and UI Theme

---
+++ Frontends/MovieApi.WebUI/Controllers/RegisterController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieApi.Dto.Dtos.UserRegisterDtos;
+
+namespace MovieApi.WebUI.Controllers
+{
+    public class RegisterController : Controller
+    {
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SignUp(CreateUserRegisterDto createUserRegisterDto) 
+        {
+
+            return RedirectToAction("SignIn","Login");
+        }
+    }
+}
