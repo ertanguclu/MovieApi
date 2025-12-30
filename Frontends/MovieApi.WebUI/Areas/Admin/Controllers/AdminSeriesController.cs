72f5ddf562bdec14a0893ba90280c22d04db6ba3
Ertan GÜÇLÜ <[ ertan.guclu@std.yildiz.edu.tr]>
2025-12-30 19:23:22 +0300

    Added Series controller and CRUD actions

---
+++ Frontends/MovieApi.WebUI/Areas/Admin/Controllers/AdminSeriesController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieApi.Dto.Dtos.AdminDtos.AdminSeriesDtos;
+using Newtonsoft.Json;
+
+namespace SeriesApi.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminSeriesController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminSeriesController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> SeriesList()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7016/api/Series");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<AdminResultSeriesDto>>(jsonData);
+                return View(values);
+            }
+
+            return View();
+        }
+        [HttpGet]
+        public IActionResult CreateSeries()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateSeries(CreateAdminSeriesDto createAdminSeriesDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createAdminSeriesDto);
+            StringContent stringContent = new StringContent(jsonData, System.Text.Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7016/api/Seriess", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("SeriesList");
+            }
+            return View();
+        }
+        public async Task<IActionResult> DeleteSeries(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync("https://localhost:7016/api/Seriess?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("SeriesList", "AdminSeries", new { area = "Admin" });
+            }
+            return View();
+        }
+    }
+}
--- Presentation/MovieApi.WebApi/Program.cs
+++ Presentation/MovieApi.WebApi/Program.cs
@@ -1,34 +1,18 @@
-using Microsoft.AspNetCore.Identity;
-using Microsoft.OpenApi.Models;
-using MovieApi.Application.Features.CQRSDesignPattern.Handlers.CategoryHandlers;
-using MovieApi.Application.Features.CQRSDesignPattern.Handlers.MovieHandlers;
-using MovieApi.Application.Features.CQRSDesignPattern.Handlers.SeriesHandlers;
-using MovieApi.Application.Features.CQRSDesignPattern.Handlers.UserRegisterHandlers;
-using MovieApi.Application.Features.MediatorDesignPattern.Handlers.TagHandlers;
 using MovieApi.Persistence.Context;
-using MovieApi.Persistence.Identity;
 using MovieApi.WebApi.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddDbContext<MovieContext>();
 
-builder.Services.AddApplicationServices();
-
-
-builder.Services.AddIdentity<AppUser, IdentityRole>().AddEntityFrameworkStores<MovieContext>();
-
-builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(GetTagQueryHandler).Assembly));
-
+builder.Services
+    .AddApplicationServices()
+    .AddIdentityServices()
+    .AddMediatRServices()
+    .AddSwaggerServices();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My Api", Version = "v1" });
-});
 
 var app = builder.Build();
 
