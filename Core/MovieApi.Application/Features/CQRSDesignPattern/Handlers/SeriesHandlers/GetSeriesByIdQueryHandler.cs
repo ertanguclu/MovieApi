43c51a584b55438c94f9569c0ca122e77f5f6ead
Ertan GÜÇLÜ <[ ertan.guclu@std.yildiz.edu.tr]>
2025-12-30 17:53:10 +0300

    Added CQRS classes for Series entity

---
+++ Core/MovieApi.Application/Features/CQRSDesignPattern/Handlers/SeriesHandlers/GetSeriesByIdQueryHandler.cs
@@ -0,0 +1,43 @@
+﻿using MovieApi.Application.Features.CQRSDesignPattern.Queries.MovieQueries;
+using MovieApi.Application.Features.CQRSDesignPattern.Queries.SeriesQueries;
+using MovieApi.Application.Features.CQRSDesignPattern.Result.MovieResult;
+using MovieApi.Application.Features.CQRSDesignPattern.Result.SeriesResult;
+using MovieApi.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.CQRSDesignPattern.Handlers.SeriesHandlers
+{
+    public class GetSeriesByIdQueryHandler
+    {
+        private readonly MovieContext _context;
+
+        public GetSeriesByIdQueryHandler(MovieContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<GetSeriesByIdQueryResult> Handle(GetSeriesByIdQuery query)
+        {
+            var value = await _context.Serieses.FindAsync(query.SeriesId);
+            return new GetSeriesByIdQueryResult
+            {
+                SeriesId = value.SeriesId,
+                Title = value.Title,
+                CoverImageUrl = value.CoverImageUrl,
+                Rating = value.Rating,
+                Description = value.Description,
+                FirstAirDate = value.FirstAirDate,
+                CreatedYear = value.CreatedYear,
+                AverageEpisodeDuration = value.AverageEpisodeDuration,
+                SeasonCount = value.SeasonCount,
+                EpisodeCount = value.EpisodeCount,
+                Status = value.Status,
+                CategoryId = value.CategoryId
+            };
+        }
+    }
+}
---
+++ Core/MovieApi.Application/Features/CQRSDesignPattern/Handlers/SeriesHandlers/GetSeriesQueryHandler.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieApi.Application.Features.CQRSDesignPattern.Result.MovieResult;
+using MovieApi.Application.Features.CQRSDesignPattern.Result.SeriesResult;
+using MovieApi.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.CQRSDesignPattern.Handlers.SeriesHandlers
+{
+    public class GetSeriesQueryHandler
+    {
+        private readonly MovieContext _context;
+
+        public GetSeriesQueryHandler(MovieContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<GetSeriesQueryResult>> Handle()
+        {
+            var values = await _context.Serieses.ToListAsync();
+            return values.Select(x => new GetSeriesQueryResult
+            {
+                SeriesId = x.SeriesId,
+                Title = x.Title,
+                CoverImageUrl = x.CoverImageUrl,
+                Rating = x.Rating,
+                Description = x.Description,
+                FirstAirDate = x.FirstAirDate,
+                CreatedYear = x.CreatedYear,
+                AverageEpisodeDuration = x.AverageEpisodeDuration,
+                SeasonCount = x.SeasonCount,
+                EpisodeCount = x.EpisodeCount,
+                Status = x.Status,
+                CategoryId = x.CategoryId
+            }).ToList();
+
+        }
+    }
+}
